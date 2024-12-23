db164d2c4a35f7bec22ab224aed780f11cced644
swati <swati2003mry@gmail.com>
2024-12-23 23:47:52 +0530

    updated code to iterative manner

--- fib1.py
+++ fib1.py
@@ -1,6 +1,7 @@
 def fibonacci(n):
-    if n <= 1:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
 
 print(fibonacci(10))
